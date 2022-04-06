77b862f22ac45ca9922b4f5e293dacb0284953fe
montanojd <gpapplica@gmail.com>
2022-04-05 23:13:29 -0500

    first commit

---
+++ precache-manifest.8c1f8da210d67e16ac4644d089d086d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54f24883a5fe139e6ffdecdbad07d77",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70c16aa95e835ca7900a",
+    "url": "./static/css/main.c842291f.chunk.css"
+  },
+  {
+    "revision": "1d2139eae1000e3f1b57",
+    "url": "./static/js/2.8c61cad5.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "./static/js/2.8c61cad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c16aa95e835ca7900a",
+    "url": "./static/js/main.03b9d0f8.chunk.js"
+  },
+  {
+    "revision": "e71fbc78101dfb770eec",
+    "url": "./static/js/runtime-main.a0dcb9d0.js"
+  },
+  {
+    "revision": "660f500a386e6f6b598c8cb5374180bf",
+    "url": "./static/media/Icon.660f500a.png"
+  },
+  {
+    "revision": "296bde98c00cce5aaf6f74928c6ccd42",
+    "url": "./static/media/woof.296bde98.png"
+  }
+]);
